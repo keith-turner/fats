e2ac41da1043bad0fb5e2cc40803b05263b85679
Keith Turner <kturner@apache.org>
2015-06-22 15:19:46 -0400

    Added script to setup RW

---
+++ setup-rw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+cd `ls -d /home/ec2-user/install/accumulo-*`
+cd test/system/randomwalk/
+
+cp conf/logger.xml.example conf/logger.xml
+cp conf/randomwalk.conf.example conf/randomwalk.conf
+
+sed -i '/ZOOKEEPERS=/d' conf/randomwalk.conf
+
+echo ZOOKEEPERS=`grep io.fluo.client.accumulo.zookeepers= /home/ec2-user/install/fluo-cluster/conf/fluo.properties | cut -f 2 -d =` >> conf/randomwalk.conf
+
+sed -i 's/\(.*edge.*id="Security.xml".*\)/<!-- \1  -->/' conf/modules/All.xml
+
+cp ../../../conf/slaves conf/walkers
