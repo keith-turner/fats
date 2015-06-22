1a58a6f9ac7562c424b5a33655a9156cba69971d
Keith Turner <kturner@apache.org>
2015-06-22 15:27:21 -0400

    set instance and install pssh

--- setup-rw.sh
+++ setup-rw.sh
@@ -9,9 +9,15 @@ cp conf/logger.xml.example conf/logger.xml
 cp conf/randomwalk.conf.example conf/randomwalk.conf
 
 sed -i '/ZOOKEEPERS=/d' conf/randomwalk.conf
+sed -i '/INSTANCE=/d' conf/randomwalk.conf
 
 echo ZOOKEEPERS=`grep io.fluo.client.accumulo.zookeepers= /home/ec2-user/install/fluo-cluster/conf/fluo.properties | cut -f 2 -d =` >> conf/randomwalk.conf
+echo INSTANCE=`grep io.fluo.client.accumulo.instance= /home/ec2-user/install/fluo-cluster/conf/fluo.properties | cut -f 2 -d =` >> continuous-env.sh
+
 
 sed -i 's/\(.*edge.*id="Security.xml".*\)/<!-- \1  -->/' conf/modules/All.xml
 
 cp ../../../conf/slaves conf/walkers
+
+sudo yum -y install pssh
+
